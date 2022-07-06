c93509cb05223314987f0a6e7e45f010c299e8e8
Nimsara Fernando <nimsara66@gmail.com>
2022-07-06 10:59:31 +0530

    Implement folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import './App.scss'
+import { Navbar } from './components'
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+
 const App = () => {
-  return <h1>App</h1>
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
